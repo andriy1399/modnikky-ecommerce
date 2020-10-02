309e3182e10cbf25569fe14bcfef6a2333f8fcf6
andriy1399 <andrijn333@gmail.com>
2020-10-03 00:59:20 +0300

    added basic routing and desktop header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShopComponent } from './pages/shop/shop.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SaleComponent } from './pages/sale/sale.component';
+import { ProductComponent } from './pages/product/product.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { BagComponent } from './pages/bag/bag.component';
+import { NewArrivalsComponent } from './pages/new-arrivals/new-arrivals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'sale', component: SaleComponent },
+  { path: 'new-arrivals', component: NewArrivalsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'bag', component: BagComponent },
+  { path: 'product/:category/:id', component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
