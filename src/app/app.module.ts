47dcbb5c55f72ecd527054c7fc9be650e8eb9774
andriy1399 <andrijn333@gmail.com>
2020-10-02 15:36:30 +0300

    created components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { BagModalComponent } from './components/bag-modal/bag-modal.component';
+import { NewArrivalsComponent } from './pages/new-arrivals/new-arrivals.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { SaleComponent } from './pages/sale/sale.component';
+import { ProductComponent } from './pages/product/product.component';
+import { BagComponent } from './pages/bag/bag.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignInComponent,
+    BagModalComponent,
+    NewArrivalsComponent,
+    HomeComponent,
+    ShopComponent,
+    SaleComponent,
+    ProductComponent,
+    BagComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
