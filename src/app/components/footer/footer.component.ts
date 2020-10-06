07a3621a0c21b22822443e0dc7a19801fa63007e
andriy1399 <andrijn333@gmail.com>
2020-10-06 17:03:31 +0300

    create footer and panel component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IPanel } from '../../shared/interfaces/panel.interface';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  footerInfo: Array<IPanel> = [
+    {
+      pTitle: 'CUSTOMER SERVICE', pList: [
+        { text: 'contact' },
+        { text: 'TRACK ORDER' },
+        { text: 'DELIVERY & RETURNS' },
+        { text: 'PAYMENT' },
+        { text: 'MAKE A RETURN' },
+        { text: 'MAKE A RETURN' },
+        { text: 'FAQ' }
+      ]
+    },
+    {
+      pTitle: 'info', pList: [
+        { text: 'Gift vouchers' },
+        { text: 'size guide' },
+        { text: 'careers at modnikky' },
+        { text: 'about us' },
+        { text: 'legal policies' },
+      ]
+    },
+    {
+      pTitle: 'follow us', pList: [
+        {
+          text: 'facebook',
+          img: 'https://svgsilh.com/svg/667456.svg',
+          href: 'https://uk-ua.facebook.com/'
+        },
+        {
+          text: 'linkedin',
+          img: 'https://www.flaticon.com/svg/static/icons/svg/38/38669.svg',
+          href: 'https://www.linkedin.com/'
+        },
+        {
+          text: 'instagram',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Instagram_font_awesome.svg/1024px-Instagram_font_awesome.svg.png',
+          href: 'https://www.instagram.com'
+        },
+      ]
+    },
+    {
+      pTitle: 'Contacts', pList: [
+        { text: 'hello@modnikky.com' },
+        { text: '+380505012333' },
+        { text: 'Shevchencka 23, Lviv' },
+      ]
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
